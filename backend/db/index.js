2830d22b51df070720f782ec0d0128e969b6f323
belayhun-arage <belayhun24arage@gmail.com>
2024-04-30 09:45:18 +0300

    fix ssl issue

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,7 +1,10 @@
 import pg from 'pg'
 
 const db = new pg.Pool({
-    connectionString:"postgres://usertodos:odfxgX4nfBLu8LXAka2cHuiycqLybZAZ@dpg-coo8s7ev3ddc738itpeg-a.oregon-postgres.render.com/todos_mpxo"
+    connectionString:"postgres://usertodos:odfxgX4nfBLu8LXAka2cHuiycqLybZAZ@dpg-coo8s7ev3ddc738itpeg-a.oregon-postgres.render.com/todos_mpxo",
+    ssl: {
+        rejectUnauthorized: false, // Ignore self-signed certificates
+    },
 })
 //     {
 //     host:process.env.DB_HOST,
